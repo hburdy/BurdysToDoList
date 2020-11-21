c454987ba2d1594fdad809156b443f52006a7424
H. Burdy <58144965+hburdy@users.noreply.github.com>
2020-11-20 18:17:42 -0600

    Second Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listeners
+todoButton.addEventListener('click', addTodo);
+
+//functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+}
